fad263e79c4208e4869e946411fa1a89741f1fb0
Lucas Santos <lucas201273@gmail.com>
2025-06-19 20:54:13 -0300

    test: ensure HttpAdapter calls post without body

--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -15,7 +15,8 @@ class HttpAdapter{
   const HttpAdapter({ required this.httpClient});
   
   Future<void> request({ required String url, required String method, Map? body}) async {
-    await httpClient.post(Uri.parse(url), headers: {'content-type': 'application/json', 'accept': 'application/json'}, body: jsonEncode(body) );
+    final jsonBody = body != null ? jsonEncode(body) : null;
+    await httpClient.post(Uri.parse(url), headers: {'content-type': 'application/json', 'accept': 'application/json'}, body: jsonBody);
   }
   
 }
@@ -39,5 +40,13 @@ void main(){
       await sut.request(url: url , method: 'post', body: {'any_key': 'any_value'});
       verify(mockClient.post(Uri.parse(url), headers:headers , body: jsonEncode({'any_key': 'any_value'}) ));
     });
+
+
+    test('Should call post with without  body', ()async {
+      when(mockClient.post(Uri.parse(url), headers: anyNamed('headers'))).thenAnswer((_) async => Response('ok', 200));
+      await sut.request(url: url , method: 'post');
+      verify(mockClient.post(Uri.parse(url), headers:headers));
+    });
+    
   });
 }
