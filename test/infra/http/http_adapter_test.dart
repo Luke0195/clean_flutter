129d29980c64d8e455c8421fbb23edfbce07791c
Lucas Santos <lucas201273@gmail.com>
2025-06-19 20:38:17 -0300

    test: should calls HttpClient with correct headers

--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -13,7 +13,7 @@ class HttpAdapter{
   const HttpAdapter({ required this.httpClient});
   
   Future<void> request({ required String url, required String method, Map? json}) async {
-    await httpClient.post(Uri.parse(url));
+    await httpClient.post(Uri.parse(url), headers: {'Content-Type': 'application/json', 'Accept': 'application/json'});
   }
   
 }
@@ -24,11 +24,12 @@ void main(){
   group('post', (){
     test('Should call post with correct values', ()async {
       final url = faker.internet.httpUrl();
+      final headers = {'Content-Type': 'application/json', 'Accept': 'application/json'};
       final client = MockClient();
-      when(client.post(Uri.parse(url))).thenAnswer((_) async => Response('ok', 200));
+      when(client.post(Uri.parse(url), headers: anyNamed('headers'))).thenAnswer((_) async => Response('ok', 200));
       final sut = HttpAdapter(httpClient: client);
       await sut.request(url: url , method: 'post');
-      verify(client.post(Uri.parse(url)));
+      verify(client.post(Uri.parse(url), headers: headers));
     });
   });
 }
