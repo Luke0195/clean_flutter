35ce50511b309f2574e979f51468758502f87a3c
Lucas Santos <lucas201273@gmail.com>
2025-06-20 05:01:58 -0300

    test: move post httpClient to setup method

--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -39,28 +39,28 @@ void main(){
     });
     
   group('post', (){
+      PostExpectation mockRequest()=> when(mockClient.post(any, headers: anyNamed('headers'), body: anyNamed('body')));
+    setUp((){
+      mockRequest().thenAnswer((_) async => Response(jsonEncode({'any_key': 'any_value'}), 200));
+    });
     test('Should call post with correct values', ()async {
-      when(mockClient.post(any, headers: anyNamed('headers'), body: anyNamed('body'))).thenAnswer((_) async => Response(jsonEncode({'any_key': 'any_value'}), 200));
       await sut.request(url: url , method: 'post', body: {'any_key': 'any_value'});
       verify(mockClient.post(Uri.parse(url), headers:headers , body: jsonEncode({'any_key': 'any_value'}) ));
     });
 
 
     test('Should call post with without  body', ()async {
-      when(mockClient.post(any, headers: anyNamed('headers'))).thenAnswer((_) async => Response(jsonEncode({'any_key': 'any_value'}), 200));
       await sut.request(url: url , method: 'post');
       verify(mockClient.post(Uri.parse(url), headers:headers));
     });
 
     test('Should returns data if post returns 200',  ()async {
-      when(mockClient.post(any, headers: anyNamed('headers')))
-      .thenAnswer((_) async => Response(jsonEncode({'any_key': 'any_value'}), 200));
       final response = await sut.request(url: url, method: 'post');
       expect(response, {'any_key': 'any_value'});
     });
 
     test('Should return empty map if post returns 200 with no data', ()async {
-      when(mockClient.post(any, headers: anyNamed('headers'))).thenAnswer((_) async => Response('', 200));
+      mockRequest().thenAnswer((_) async => Response('', 200));
       final response = await sut.request(url: url, method: 'post');
       expect(response, isEmpty);
       expect(response, isA<Map>());
